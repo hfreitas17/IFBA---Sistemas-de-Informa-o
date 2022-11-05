17778ced822327e4d5a245f6dbd4857044363700
Hamilton Freitas <hamilton.freitas@hotmail.com.br>
2022-11-05 08:06:18 -0300

    inicio

--- 3 Semestre/Estruturas de Dados/Fila em Java/Queue.java
+++ 3 Semestre/Estruturas de Dados/Fila em Java/Queue.java
@@ -1,1 +1,29 @@
-//Trabalho de Estrutura de Dados
+
+/*
+Trabalho Em Grupo de Estrutura de Dados 1° Unidade - Grupo I
+Professor: Cláudio Rodolfo Sousa de Oliveira
+Alunos:
+- Gabriela
+- Gustavo Everton
+- Ariane
+- Mariana
+- Brendo
+- Hamilton
+*/
+
+import java.util.Queue;
+
+public class principal {
+
+    public static void main(String[] args) {
+
+        Queue<> filaDePessoas = Queue();
+
+        
+
+
+    }
+
+
+
+}
